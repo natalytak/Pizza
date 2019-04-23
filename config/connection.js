d998cd6718a63c8f530751d93ece1a29f0f17a25
Tasha <natalytak@yahoo.com>
2019-04-23 14:51:26 -0400

    final

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,26 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'pizza_db'
+// });
+
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '',
     database: 'pizza_db'
+},
+{
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql"
+    }
+
 });
 
 connection.connect(function(err) {
